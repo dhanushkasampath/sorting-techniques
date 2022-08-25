90ee443405f34fc5b09394ca79742cba490264a7
dsampath <dsampath@wiley.com>
2022-08-25 19:02:51 +0530

    first commit

---
+++ src/main/java/com/learnings/sortingtechniques/SortingTechniquesApplication.java
@@ -0,0 +1,13 @@
+package com.learnings.sortingtechniques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingTechniquesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingTechniquesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnings/sortingtechniques/SortingTechniquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnings.sortingtechniques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingTechniquesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
